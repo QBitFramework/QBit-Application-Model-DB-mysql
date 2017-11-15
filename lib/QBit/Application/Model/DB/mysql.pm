05af4c2fa9cb1b935764e5d41a61e208960cd58e
Igor Sverdlov <igor-sverdlov@mail.ru>
2017-11-16 01:01:38 +0300

    support comments and deleted method get_query_id

--- lib/QBit/Application/Model/DB/mysql.pm
+++ lib/QBit/Application/Model/DB/mysql.pm
@@ -17,9 +17,9 @@ sub filter {
 }
 
 sub query {
-    my ($self) = @_;
+    my ($self, %opts) = @_;
 
-    return QBit::Application::Model::DB::mysql::Query->new(db => $self);
+    return QBit::Application::Model::DB::mysql::Query->new(db => $self, %opts);
 }
 
 sub transaction {
@@ -46,6 +46,12 @@ sub _do {
     };
 
     return $res;
+}
+
+sub kill_query {
+    my ($self, $query_id) = @_;
+
+    $self->_do("KILL QUERY $query_id");
 }
 
 sub _get_table_class {
@@ -106,3 +112,178 @@ sub _is_connection_error {
 }
 
 TRUE;
+
+__END__
+
+=encoding utf8
+
+=head1 Name
+
+QBit::Application::Model::DB::mysql - Class for working with MySQL DB.
+
+=head1 Description
+
+Class for working with MySQL DB. It's not ORM.
+
+=head1 GitHub
+
+https://github.com/QBitFramework/QBit-Application-Model-DB-mysql
+
+=head1 Install
+
+=over
+
+=item *
+
+cpanm QBit::Application::Model::DB::mysql
+
+=item *
+
+apt-get install libqbit-application-model-db-mysql-perl (http://perlhub.ru/)
+
+=back
+
+=head1 Package methods
+
+=head2 filter
+
+B<Arguments:>
+
+=over
+
+=item *
+
+B<$filter> - filter (perl variables)
+
+=item *
+
+B<%opts> - additional options
+
+=over
+
+=item *
+
+B<type> - type (AND/OR NOT)
+
+=back
+
+=back
+
+B<Return values:>
+
+=over
+
+=item
+
+B<$filter> - object (QBit::Application::Model::DB::Filter)
+
+=back
+
+B<Example:>
+
+  my $filter = $app->db->filter([id => '=' => \23]);
+
+=head2 query
+
+Create and returns a new query object.
+
+B<Arguments:>
+
+=over
+
+=item *
+
+B<%opts> - options
+
+=over
+
+=item *
+
+comment
+
+=item
+
+without_check_fields
+
+=item
+
+without_table_alias
+
+=back
+
+=back
+
+B<Return values:>
+
+=over
+
+=item
+
+B<$query> - object (QBit::Application::Model::DB::mysql::Query)
+
+=back
+
+B<Example:>
+
+  my $query = $app->db->query(comment => 'example query');
+
+=head2 kill_query
+
+B<Arguments:>
+
+=over
+
+=item
+
+B<$query_id> - number (ID query)
+
+=back
+
+B<Return values:>
+
+=over
+
+=item
+
+B<$res> - Returns the number of rows affected or undef on error.
+
+A return value of -1 means the number of rows is not known, not applicable, or not available.
+
+=back
+
+B<Example:>
+
+  my $res = $app->db->kill_query(35); #SQL: KILL QUERY 35;
+
+=head2 transaction
+
+B<Arguments:>
+
+=over
+
+=item
+
+B<$sub> - reference to sub
+
+=back
+
+B<Example:>
+
+  $app->db->transaction(sub {
+      # work with db
+      ...
+  });
+
+=head1 Internal packages
+
+=over
+
+=item B<L<QBit::Application::Model::DB::mysql::Field>> - class for MySQL fields;
+
+=item B<L<QBit::Application::Model::DB::mysql::Query>> - class for MySQL queries;
+
+=item B<L<QBit::Application::Model::DB::mysql::Table>> - class for MySQL tables;
+
+=back
+
+=cut
